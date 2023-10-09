0a551c3870abb0668d5b558a7410a34bf98c08d1
hanantariq10 <hanantariq1010@gmail.com>
2023-10-08 21:57:46 -0400

    main: added basic functionality to Item.java

--- Item.java
+++ Item.java
@@ -1,3 +1,54 @@
 public class Item{
-    
+
+    // member variables
+    private String name;
+    private String type;
+    private String description;
+
+
+    // constructor
+    public Item(String name, String type, String description){
+        this.name = name;
+        this.type = type;
+        this.description = description;
+    }
+
+
+    // getters
+    public String getName() {
+        return name;
+    }
+
+
+    public String getType() {
+        return type;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+
+    // setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+    // member methods
+    public String toString(){
+        String output = String.format("%s[%s]: %s", name, type, description);
+        return output;
+    }
 }
