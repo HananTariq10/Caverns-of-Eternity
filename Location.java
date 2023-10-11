00f5dbf4b98a36338706b5eb1094a1bb31ca5687
hanantariq10 <hanantariq1010@gmail.com>
2023-10-10 22:53:00 -0400

    main: made few corrections to Location class.

--- Location.java
+++ Location.java
@@ -11,7 +11,7 @@ public class Location {
     public Location(String name, String description){
         this.name = name;
         this.description = description;
-        this.items = new ArrayList<>();
+        this.items = new ArrayList<Item>();
     }
 
     //getter
